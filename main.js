bd1dd7e6556aceaeef78d8eca202b88a79b27ecc
Bubby123456 <78021075+Bubby123456@users.noreply.github.com>
2021-03-24 11:16:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function showmenu(){
+    var pizza1=("1.Chicken Barbeque Pizza");
+    var pizza2=("2.Chicken Tandoori Pizza");
+    var pizza3=("3.Chicken Tikka Pizza");
+    var pizza4=("4.Golden Veg Pizza");
+    var pizza5=("5.Paneer Tikka Pizza");
+
+    document.getElementById("1").innerHTML=pizza1;
+    document.getElementById("2").innerHTML=pizza2;
+    document.getElementById("3").innerHTML=pizza3;
+    document.getElementById("4").innerHTML=pizza4;
+    document.getElementById("5").innerHTML=pizza5;
+}
